f002470aa4bcb8c8711405e80435038ef63eb951
abdellah khouden <khoudenak@gmail.com>
2024-12-07 00:15:09 +0100

    Add Produit model, factory, migration, and seeder; update DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([
+            CategorieSeeder::class,
+            ProduitSeeder::class,
+        ]);
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        
     }
 }
